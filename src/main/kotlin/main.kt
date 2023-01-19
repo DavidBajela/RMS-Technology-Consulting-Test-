cf3418db8709237bb14c40108eec79bf2ff4faa0
David Bajela <dbajela@gmail.com>
2023-01-19 12:43:51 +0000

    init

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,25 +1,40 @@
-
 fun findProductv1(grid: Array<IntArray>, length: Int): Long {
     var loops = 0
     var product = 0L
     var a: Int
     var b: Int
     var c: Int
-    var d:Int
-    for (x in 0..length - 3) {
-        for (y in 0..length - 3) {
-            a = grid[x][y] * grid[x + 1][x] * grid[x + 2][y]
-            b = grid[x][y] * grid[x][y + 1] * grid[x][y + 2]
-            c = grid[x][y] * grid[x + 1][y + 1] * grid[x + 2][y + 2]
-            d= grid[x+2][y] * grid[x + 1][y + 1] * grid[x][y + 2]
+    var d: Int
+    for (y in 0 until length) {
+        for (x in 0 until length) {
+            //check horizontals
+            if (x + 2 < length) {
+                a = grid[x][y] * grid[x + 1][y] * grid[x + 2][y]
+                loops++
+            } else a = 0
+            //check verticals
+            if (y + 2 < length) {
+                b = grid[x][y] * grid[x][y + 1] * grid[x][y + 2]
+                loops++
+            } else b = 0
+            //check diagonals
+            if (y + 2 < length && x + 2 < length) {
+                c = grid[x][y] * grid[x + 1][y + 1] * grid[x + 2][y + 2]
+                d = grid[x + 2][y] * grid[x + 1][y + 1] * grid[x][y + 2]
+                loops += 2
+            } else {
+                c = 0
+                d = 0
+            }
             product = product.coerceAtLeast(d.coerceAtLeast(a).coerceAtLeast(b.coerceAtLeast(c)).toLong())
-            loops++
+
         }
     }
+    println("number of cycles = $loops")
     return product
 }
 
-
+//288
 fun main() {
     val gid = arrayOf(
         intArrayOf(8, 2, 22, 97, 35, 15, 0, 40, 0, 75),
@@ -31,7 +46,7 @@ fun main() {
         intArrayOf(32, 98, 81, 28, 68, 2, 62, 12, 20, 95),
         intArrayOf(67, 26, 20, 68, 2, 62, 12, 20, 95, 63),
         intArrayOf(24, 55, 58, 5, 66, 73, 99, 26, 97, 17),
-        intArrayOf(21, 36, 23, 9, 75, 0, 76, 44, 20, 45),
+        intArrayOf(21, 36, 23, 9, 75, 0, 76, 44, 20, 45)
     )
     print(
         findProductv1(gid, 10)
